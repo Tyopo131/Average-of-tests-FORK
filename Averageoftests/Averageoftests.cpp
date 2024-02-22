70867438ea3dce7ce3ed6f4e92fef5db5e8fb32c
T-yopo <samecob62@gmail.com>
2024-02-22 10:24:51 +0000

    Revert "fix"
    This reverts commit 3b3ff319c6a45247700a4cb4158087be42b89e03.

--- Averageoftests/Averageoftests.cpp
+++ Averageoftests/Averageoftests.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include "standaloneExitChoice.h"
+#ifdef _DEBUG
+#pragma comment(lib, "standaloneExitChoice.Debug.lib")
+#else
+#pragma comment(lib, "standaloneExitChoice.Release.lib")
+#endif
 using namespace std;
 
 int main() {
